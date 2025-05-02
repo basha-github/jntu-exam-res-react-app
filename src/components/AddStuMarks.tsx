23ac43c47a987044aae347ed5d1f69e52c3489e5
sharifabee <sharifabee.alur@gmail.com>
2025-05-02 11:37:19 +0530

    marks form

--- src/components/AddStuMarks.tsx
+++ src/components/AddStuMarks.tsx
@@ -1,118 +1,111 @@
+import axios from "axios";
 import React, { useState } from "react";
+
+import '../css/stures.css'
 
 export default function AddStuMarks() {
 
-const [rollNo,setRollNo] = useState("");
 
-const getRollNum = (e:any)=>{
-//    console.log(e.target.value);
+    const [message,setMessage] = useState("");
+
+  const [rollNo, setRollNo] = useState("");
+
+  const getRollNum = (e: any) => {
+    //    console.log(e.target.value);
     setRollNo(e.target.value);
-  }
+  };
 
-  const [name,setName] = useState("");
+  const [name, setName] = useState("");
 
-const getName = (e:any)=>{
-  //  console.log(e.target.value);
+  const getName = (e: any) => {
+    //  console.log(e.target.value);
     setName(e.target.value);
-  }
+  };
 
-  const [college,setCollege] = useState("");
+  const [college, setCollege] = useState("");
 
-const getCollege = (e:any)=>{
+  const getCollege = (e: any) => {
     //console.log(e.target.value);
     setCollege(e.target.value);
-  }
+  };
 
-  const [course,setCourse] = useState("");
+  const [course, setCourse] = useState("");
 
-const getCourse = (e:any)=>{
+  const getCourse = (e: any) => {
     //console.log(e.target.value);
     setCourse(e.target.value);
-  }
+  };
 
-  const [marks,setMarks] = useState("");
+  const [marks, setMarks] = useState("");
 
-const getMarks = (e:any)=>{
+  const getMarks = (e: any) => {
     //console.log(e.target.value);
     setMarks(e.target.value);
-  }
+  };
 
-  const [fee,setFee] = useState("");
+  const [fee, setFee] = useState("");
 
-const getFee = (e:any)=>{
+  const getFee = (e: any) => {
     //console.log(e.target.value);
     setFee(e.target.value);
-  }
-
-
-
+  };
 
   const addStu = (e: FormDataEvent) => {
     e.preventDefault();
     console.log("hello");
-    console.log("rollno--->"+rollNo);
-    console.log("name----->"+name);
-    console.log("college--->"+college);
-    console.log("course--->"+course);
-    console.log("marks--->"+marks);
-    console.log("fee--->"+fee);
-    
+    console.log("rollno--->" + rollNo);
+    console.log("name----->" + name);
+    console.log("college--->" + college);
+    console.log("course--->" + course);
+    console.log("marks--->" + marks);
+    console.log("fee--->" + fee);
 
+    const student = {
+      rollNo: rollNo,
+      name: name,
+      college: college,
+      course: course,
+      marks: marks,
+      fee: fee,
+    };
+
+    axios.post("http://localhost:8080/jntu/exam/add/marks", student)
+    .then((res) => {
+      console.log("res from spring boot-->" + res.data);
+      setMessage(res.data);
+      setRollNo(" ");
+    });
   };
 
- 
   return (
     <div>
       <form>
+        <div className="stuMarks">
+            <h2>{message}</h2>
+        </div>
         <div className="mb-3">
           <label className="form-label">Roll Number</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={getRollNum}
-            />
+          <input type="text" className="form-control" onChange={getRollNum} />
         </div>
         <div className="mb-3">
           <label className="form-label">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={getName}
-           
-           />
+          <input type="text" className="form-control" onChange={getName} />
         </div>
         <div className="mb-3">
           <label className="form-label">College</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={getCollege}
-           />
+          <input type="text" className="form-control" onChange={getCollege} />
         </div>
         <div className="mb-3">
           <label className="form-label">Course</label>
-          <input
-            type="text"
-            className="form-control"
-           onChange={getCourse}
-           
-           />
+          <input type="text" className="form-control" onChange={getCourse} />
         </div>
         <div className="mb-3">
           <label className="form-label">Marks</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={getMarks}
-           />
+          <input type="text" className="form-control" onChange={getMarks} />
         </div>
         <div className="mb-3">
           <label className="form-label">Fee</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={getFee}
-            />
+          <input type="text" className="form-control" onChange={getFee} />
         </div>
         <button onClick={addStu} type="submit" className="btn btn-primary">
           Submit
